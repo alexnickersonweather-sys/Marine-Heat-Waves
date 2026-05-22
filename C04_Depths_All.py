293b732591b66ecb1a4da5e20a6fe0419ae2f72e
alexnickersonweather-sys <alexnickersonweather@gmail.com>
2026-05-22 15:15:59 -0400

    Update C04_Depths_All.py

--- C04_Depths_All.py
+++ C04_Depths_All.py
@@ -79,3 +79,48 @@ plt.show()
 # save the figure
 file2save = folder + "Counts_Depth_All.png"
 fig.savefig(file2save,bbox_inches='tight',dpi=300)
+        # extract data from netCDF files
+        file2open = "Data/" + t + "/L00N/" + stat["Reg"].strip() + ".nc"
+    
+        ds = xr.open_dataset(file2open)
+        df = ds.to_dataframe()
+        df = df.reset_index()
+        ds.close()
+        
+        z = np.nanmean(df["z"])
+        depth.append(z)
+    
+# my personal preferences for font
+plt.rcParams["font.family"] = "serif"
+plt.rcParams['font.serif'] = ['Times New Roman']
+plt.rcParams['font.size'] = 12
+
+# generate the figure window
+fig, ax = plt.subplots()
+plt.draw()
+
+# axis labels; rotate the x-axis labels 90 degrees
+ax.set_ylabel(r'Count')
+
+# x-axis limits and ticks
+ax.set_xlim(0,160)
+ax.set_xticks(np.arange(0,160.5,step=10))
+ax.xaxis.set_minor_locator(MultipleLocator(5))
+ax.set_xticklabels(np.arange(0,160.5,step=10),rotation=90)
+
+# y-axis limits and ticks
+ax.set_ylim(0,700)
+ax.set_yticks(np.arange(0,701,step=100))
+ax.yaxis.set_minor_locator(MultipleLocator(50))
+ax.set_yticklabels(np.arange(0,701,step=100))
+
+# plot title
+ax.set_title("Number of Stations by Depth")
+
+# plot the data
+ax.hist(-np.array(depth), bins=np.arange(-10,160.5,step=5))
+plt.show()
+
+# save the figure
+file2save = folder + "Counts_Depth_All.png"
+fig.savefig(file2save,bbox_inches='tight',dpi=300)
