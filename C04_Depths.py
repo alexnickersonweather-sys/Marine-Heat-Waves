9ed4bc622d44aeebee82e15ec975be46b30c5ca7
alexnickersonweather-sys <alexnickersonweather@gmail.com>
2026-06-10 13:15:29 -0400

    Update and rename C04_Depths_Type.py to C04_Depths.py
    Removes redundancies in the library.

--- C04_Depths.py
+++ C04_Depths.py
@@ -26,12 +26,12 @@ folder = "Figs/Statistics/"
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-depth = []
-
 # no USF due to the multiple depths of measurement
-for t in ["FACT","NDBC","SWT"]:
+for t in ["FACT","USF"]:
     # get subset of stations
     stat_own = stat_all.loc[stat_all["Type"] == t,:]
+    
+    depth = []
     
     for i, stat in stat_own.iterrows():
         # extract data from netCDF files
@@ -42,7 +42,7 @@ for t in ["FACT","NDBC","SWT"]:
         df = df.reset_index()
         ds.close()
         
-        z = np.nanmean(df["z"])
+        z = np.min(df["z"])
         depth.append(z)
     
     # my personal preferences for font
@@ -62,18 +62,18 @@ for t in ["FACT","NDBC","SWT"]:
     ax.set_xticks(np.arange(0,160.5,step=10))
     ax.xaxis.set_minor_locator(MultipleLocator(5))
     ax.set_xticklabels(np.arange(0,160.5,step=10),rotation=90)
-        
+
     # y-axis limits and ticks
-    ax.set_ylim(0,700)
-    ax.set_yticks(np.arange(0,701,step=100))
-    ax.yaxis.set_minor_locator(MultipleLocator(50))
-    ax.set_yticklabels(np.arange(0,701,step=100))
+    ax.set_ylim(0,300)
+    ax.set_yticks(np.arange(0,301,step=50))
+    ax.yaxis.set_minor_locator(MultipleLocator(10))
+    ax.set_yticklabels(np.arange(0,301,step=50))
         
     # plot title
     ax.set_title("Number of " + t + " Stations by Depth")
     
     # plot the data
-    ax.hist(-np.array(depth), bins=np.arange(-10,160.5,step=5))
+    ax.hist(-np.array(depth),bins=np.arange(-10,160.5,step=5),color='r')
     plt.show()
     
     # save the figure
